72da63f377c5884e599e78ee4eb130c24bcd5285
Gil Lima <gilcpd2@gmail.com>
2018-02-19 23:05:41 +0000

    Created seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219201452) do
+ActiveRecord::Schema.define(version: 20180219221321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20180219201452) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "estate_id"
-    t.date "star_date"
+    t.date "start_date"
     t.date "end_date"
     t.integer "total_cost"
     t.datetime "created_at", null: false
